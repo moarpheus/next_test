d4a37f1ebebfee0cb640af58cecff3599acd9c35
JG <moarpheus@gmail.com>
2021-12-07 13:21:03 +0000

    added styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
 import NavBar from "../components/NavBar";
+import '../styles/globals.css'
+import Head from "next/head";
 
 function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>
+          My blog
+        </title>
+      </Head>
       <header>
         <NavBar />
       </header>
